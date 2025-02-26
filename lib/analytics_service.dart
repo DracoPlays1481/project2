b4868fe9373d97b81507134a7c3a6d4b1d91a4d4
DracoPlays1481 <42610817+DracoPlays1481@users.noreply.github.com>
2025-02-26 13:26:46 +0800

    Delete lib directory

--- lib/analytics_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:firebase_analytics/firebase_analytics.dart';
-class AnalyticsService {
-  final _instance = FirebaseAnalytics.instance;
-  Future<void> logLoginScreen() async {
-    await _instance.logLogin(loginMethod: 'email');
-  }
-}
